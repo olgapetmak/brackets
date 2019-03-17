2f2ffb088c7df7523b9aed44267877f972830abf
olgapetmak <olgapetmak@gmail.com>
2019-03-17 19:24:18 +0300

    feat: add code to check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let regexpStr="";
+    // get info from config
+    bracketsConfig.forEach(element => {
+        //escaping
+        let open = element[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        let close = element[1].replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        regexpStr=`${regexpStr}${open}${close}|`
+    });
+    regexpStr = regexpStr.substring(0, regexpStr.length-1); //remove |
+
+    let rex = new RegExp(`${regexpStr}`,"g")
+    
+    while(rex.test(str)){
+        str = str.replace(rex,'');
+    }
+
+    return str.length===0;
+
+
+};
